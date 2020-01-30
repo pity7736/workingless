8efc9b67ee61b24db64ddd429f1306b9a5f3beb9
Julián Cortés <pity7736@gmail.com>
2020-01-29 23:01:54 -0500

    feat: Get holidays from year

---
+++ tests/test_holidays.py
@@ -0,0 +1,116 @@
+import datetime
+
+from pytest import mark
+
+from workingless import get_holidays_from_year
+
+
+years_holidays = (
+    (
+        2020,
+        [
+            datetime.date(2020, 1, 1),
+            datetime.date(2020, 1, 6),
+            datetime.date(2020, 3, 23),
+            datetime.date(2020, 4, 5),
+            datetime.date(2020, 4, 9),
+            datetime.date(2020, 4, 10),
+            datetime.date(2020, 4, 12),
+            datetime.date(2020, 5, 1),
+            datetime.date(2020, 5, 25),
+            datetime.date(2020, 6, 15),
+            datetime.date(2020, 6, 22),
+            datetime.date(2020, 6, 29),
+            datetime.date(2020, 7, 20),
+            datetime.date(2020, 8, 7),
+            datetime.date(2020, 8, 17),
+            datetime.date(2020, 10, 12),
+            datetime.date(2020, 11, 2),
+            datetime.date(2020, 11, 16),
+            datetime.date(2020, 12, 8),
+            datetime.date(2020, 12, 25)
+        ]
+    ),
+    (
+        2015,
+        [
+            datetime.date(2015, 1, 1),
+            datetime.date(2015, 1, 12),
+            datetime.date(2015, 3, 23),
+            datetime.date(2015, 3, 29),
+            datetime.date(2015, 4, 2),
+            datetime.date(2015, 4, 3),
+            datetime.date(2015, 4, 5),
+            datetime.date(2015, 5, 1),
+            datetime.date(2015, 5, 18),
+            datetime.date(2015, 6, 8),
+            datetime.date(2015, 6, 15),
+            datetime.date(2015, 6, 29),
+            datetime.date(2015, 7, 20),
+            datetime.date(2015, 8, 7),
+            datetime.date(2015, 8, 17),
+            datetime.date(2015, 10, 12),
+            datetime.date(2015, 11, 2),
+            datetime.date(2015, 11, 16),
+            datetime.date(2015, 12, 8),
+            datetime.date(2015, 12, 25)
+        ]
+    ),
+    (
+        2000,
+        [
+            datetime.date(2000, 1, 1),
+            datetime.date(2000, 1, 10),
+            datetime.date(2000, 3, 20),
+            datetime.date(2000, 4, 16),
+            datetime.date(2000, 4, 20),
+            datetime.date(2000, 4, 21),
+            datetime.date(2000, 4, 23),
+            datetime.date(2000, 5, 1),
+            datetime.date(2000, 6, 5),
+            datetime.date(2000, 6, 26),
+            datetime.date(2000, 7, 3),
+            datetime.date(2000, 7, 3),
+            datetime.date(2000, 7, 20),
+            datetime.date(2000, 8, 7),
+            datetime.date(2000, 8, 21),
+            datetime.date(2000, 10, 16),
+            datetime.date(2000, 11, 6),
+            datetime.date(2000, 11, 13),
+            datetime.date(2000, 12, 8),
+            datetime.date(2000, 12, 25)
+        ]
+    ),
+    (
+        2023,
+        [
+            datetime.date(2023, 1, 1),
+            datetime.date(2023, 1, 9),
+            datetime.date(2023, 3, 20),
+            datetime.date(2023, 4, 2),
+            datetime.date(2023, 4, 6),
+            datetime.date(2023, 4, 7),
+            datetime.date(2023, 4, 9),
+            datetime.date(2023, 5, 1),
+            datetime.date(2023, 5, 22),
+            datetime.date(2023, 6, 12),
+            datetime.date(2023, 6, 19),
+            datetime.date(2023, 7, 3),
+            datetime.date(2023, 7, 20),
+            datetime.date(2023, 8, 7),
+            datetime.date(2023, 8, 21),
+            datetime.date(2023, 10, 16),
+            datetime.date(2023, 11, 6),
+            datetime.date(2023, 11, 13),
+            datetime.date(2023, 12, 8),
+            datetime.date(2023, 12, 25)
+        ]
+    )
+)
+
+
+@mark.parametrize('year, expected_holidays', years_holidays)
+def test_get_holidays_from_year(year, expected_holidays):
+    holidays = get_holidays_from_year(year=year)
+
+    assert list(holidays) == expected_holidays
---
+++ workingless/__init__.py
@@ -0,0 +1,1 @@
+from .api import get_holidays_from_year  # noqa: F401
